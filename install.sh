7c47d91ff6b9b91c3963bf271107bfdbd9ee959b
Sjoerd <s.braaksma@mimirconsultancy.org>
2025-10-11 16:13:50 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+PROJECT_NAME=geonovum_pointcloud
+PYTHON=3.12
+
+echo
+echo
+echo "⭐ Searching for installed conda"
+echo
+# Recover the path to conda on your machine
+# First search the default '~/miniconda3' and '~/anaconda3' paths. If
+# those do not exist, ask for user input
+CONDA_DIR=`realpath ~/miniconda3`
+if (test -z $CONDA_DIR) || [ ! -d $CONDA_DIR ]
+then
+  CONDA_DIR=`realpath ~/anaconda3`
+fi
+
+while (test -z $CONDA_DIR) || [ ! -d $CONDA_DIR ]
+do
+    echo "Could not find conda at: "$CONDA_DIR
+    read -p "Please provide your conda install directory: " CONDA_DIR
+    CONDA_DIR=`realpath $CONDA_DIR`
+done
+
+echo "Using conda conda found at: ${CONDA_DIR}/etc/profile.d/conda.sh"
+source ${CONDA_DIR}/etc/profile.d/conda.sh
+
+echo
+echo
+echo "⭐ Creating conda environment '${PROJECT_NAME}'"
+echo
+# Create deep_view_aggregation environment from yml
+conda create --name ${PROJECT_NAME} python=${PYTHON} -y
+
+# Activate the env
+source ${CONDA_DIR}/etc/profile.d/conda.sh  
+conda activate ${PROJECT_NAME}
+
+echo
+echo
+echo "⭐ Installing conda and pip dependencies"
+echo
+conda install pip nb_conda_kernels -y
+conda install laspy
+conda install shapely
+conda install hdbscan
+conda install typing
+conda install -c conda-forge jupyterlab
