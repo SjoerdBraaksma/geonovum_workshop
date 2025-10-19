829cab0091eb6ddb0c7b77b66f9b88860449ba7e
Sjoerd <s.braaksma@mimirconsultancy.org>
2025-10-19 11:17:48 +0200

    extra

--- install.sh
+++ install.sh
@@ -43,7 +43,6 @@ conda install pip nb_conda_kernels -y
 conda install laspy
 conda install open3d
 conda install shapely
-conda install hdbscan
 conda install typing
 conda install geopandas
 conda install -c conda-forge jupyterlab
